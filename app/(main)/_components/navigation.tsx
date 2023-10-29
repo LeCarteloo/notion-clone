2da25f520c5aaa91ac49f9a419098a0aadeda4be
Filip Papiernik <filippapiernik1999@gmail.com>
2023-10-29 19:31:54 +0100

    Updated navigation, added UserItem component

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -3,8 +3,9 @@
 import { cn } from '@/lib/utils';
 import { ChevronsLeft, MenuIcon } from 'lucide-react';
 import { usePathname } from 'next/navigation';
-import React, { ElementRef, useRef, useState } from 'react';
+import React, { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
+import UserItem from './user-item';
 
 const Navigation = () => {
   const pathname = usePathname();
@@ -14,7 +15,7 @@ const Navigation = () => {
   const sidebarRef = useRef<ElementRef<'aside'>>(null);
   const navbarRef = useRef<ElementRef<'div'>>(null);
   const [isResetting, setIsResetting] = useState(false);
-  const [isCollapsed, setisCollapsed] = useState(isMobile);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
   const handleMouseMove = (event: MouseEvent) => {
     if (!isResizingRef.current) return;
@@ -51,6 +52,54 @@ const Navigation = () => {
     document.addEventListener('mouseup', handleMouseUp);
   };
 
+  const handleResetWidth = () => {
+    if (!sidebarRef.current || !navbarRef.current) {
+      return;
+    }
+
+    setIsCollapsed(false);
+    setIsResetting(true);
+
+    sidebarRef.current.style.width = isMobile ? '100%' : '240px';
+    navbarRef.current.style.setProperty(
+      'width',
+      isMobile ? '0' : 'calc(100% - 240px)',
+    );
+    navbarRef.current.style.setProperty('left', isMobile ? '100%' : '240px');
+
+    setTimeout(() => setIsResetting(false), 300);
+  };
+
+  const handleCollapse = () => {
+    if (!sidebarRef.current || !navbarRef.current) {
+      return;
+    }
+
+    setIsCollapsed(true);
+    setIsResetting(true);
+
+    sidebarRef.current.style.width = '0';
+    navbarRef.current.style.setProperty('width', '100%');
+    navbarRef.current.style.setProperty('left', '0');
+
+    setTimeout(() => setIsResetting(false), 300);
+  };
+
+  useEffect(() => {
+    if (isMobile) {
+      handleCollapse();
+      return;
+    }
+
+    handleResetWidth();
+  }, [isMobile]);
+
+  useEffect(() => {
+    if (isMobile) {
+      handleCollapse();
+    }
+  }, [pathname, isMobile]);
+
   return (
     <>
       <aside
@@ -61,24 +110,27 @@ const Navigation = () => {
           isMobile && 'w-0',
         )}
       >
-        <button className="h-6 w-6 text-muted-foreground ">
+        <button
+          onClick={handleCollapse}
+          className="h-6 w-6 text-muted-foreground absolute top-3 right-2"
+        >
           <ChevronsLeft
             className={cn(
               `h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 
-              absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition`,
+              opacity-0 group-hover/sidebar:opacity-100 transition`,
               isMobile && 'opacity-100',
             )}
           />
         </button>
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
         </div>
         <div
           onMouseDown={handleMouseDown}
-          onClick={() => {}}
+          onClick={handleResetWidth}
           className="opacity-0 group-hover/sidebar:opacity-100 transiation cursor-ew-resize absolute 
         h-full w-1 bg-primary/10 right-0 top-0"
         />
@@ -92,11 +144,11 @@ const Navigation = () => {
         )}
       >
         <nav className="bg-transparent px-3 py-2 w-full">
-          <button>
-            {isCollapsed && (
+          {isCollapsed && (
+            <button onClick={handleResetWidth}>
               <MenuIcon className="h-6 w-6 text-muted-foreground" />
-            )}
-          </button>
+            </button>
+          )}
         </nav>
       </div>
     </>
